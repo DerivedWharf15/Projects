4fd2af9d0ac3b75cf83a319bbb7bbdf88847a39f
DerivedWharf15 <DerivedWharf15@protonmail.com>
2025-07-06 18:45:51 +0800

    update

--- 1-quiz/quiz.cpp
+++ 1-quiz/quiz.cpp
@@ -1,11 +1,28 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
+    string response;
+
     cout << "Math questions" << '\n';
-    cout << "DerivedWharf15's first program";
+    cout << "DerivedWharf15's first program" << '\n';
+
+    cout << ">>";
+    cin >> response;
+    cout << endl;
+
+    while (response == 'yes')
+    {
+        cout << "What is 538 + 846" << endl;
+        cout << "Do you want to continue?" << endl;
+        cin >> response;
+        cout << endl;
+    }
+
+    cout << "Program ended. Goodbye!" << endl;
 
     return 0;
 }
